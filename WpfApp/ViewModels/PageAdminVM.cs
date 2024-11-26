147384545bec802373532a1dc2fdbf9b8038ec02
Oleg <oleg@techgame.ru>
2024-11-26 14:43:28 +0300

    *add admin

--- WpfApp/ViewModels/PageAdminVM.cs
+++ WpfApp/ViewModels/PageAdminVM.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using API.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -7,13 +8,45 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using WebClient.Net;
 
 namespace ViewModels
 {
     public class PageAdminVM : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
-        //public List<ContactEntry> ContactsEntries { get; set; }
+
+        List<Order> _ordersEntries;
+        public List<Order> OrdersEntries { get => _ordersEntries; set
+            {
+                _ordersEntries = value;
+                OnPropertyChanged("OrdersEntries");
+            }
+        }
+
+        public List<BlogEntry> _blogsEntries;
+        public List<BlogEntry> BlogsEntries { get => _blogsEntries; set 
+            {
+                _blogsEntries = value;
+                OnPropertyChanged("BlogsEntries");
+            }
+        }
+
+        public List<ContactEntry> _contactsEntries;
+        public List<ContactEntry> ContactsEntries { get => _contactsEntries; set 
+            {
+                _contactsEntries = value;
+                OnPropertyChanged("ContactsEntries");
+            }
+        }
+
+        public List<ProjectEntry> _projectEntries;
+        public List<ProjectEntry> ProjectEntries { get => _projectEntries; set 
+            {
+                _projectEntries = value;
+                OnPropertyChanged("ProjectEntries");
+            }
+        }
 
         public PageAdminVM()
         {
@@ -24,11 +57,15 @@ namespace ViewModels
         {
             try
             {
-                /*
-                var data = await ApiConnector.Instance.RequestAsync($"contacts/getlist");
+                string data;
+                data = await ApiConnector.Instance.RequestAsync($"projects/getlist");
+                ProjectEntries = JsonConvert.DeserializeObject<List<ProjectEntry>>(data);
+                data = await ApiConnector.Instance.RequestAsync($"blogs/getlist");
+                BlogsEntries = JsonConvert.DeserializeObject<List<BlogEntry>>(data);
+                data = await ApiConnector.Instance.RequestAsync($"contacts/getlist");
                 ContactsEntries = JsonConvert.DeserializeObject<List<ContactEntry>>(data);
-                OnPropertyChanged("ContactsEntries");
-                */
+                data = await ApiConnector.Instance.RequestAsync($"orders/getlist");
+                OrdersEntries = JsonConvert.DeserializeObject<List<Order>>(data);
             }
             catch (Exception e)
             {
